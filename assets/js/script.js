746d4e346dd39c003363b1bb5f1d2bb0624a80b6
LETRA <pedro.pardim.pc@gmail.com>
2025-09-20 21:40:46 -0300

    feat: add basic functionality

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  const logo = document.querySelector('.logo');
+  const links = document.querySeclectorAll('.nav-links a');
+
+  const scrollPosition = window.scrollY;
+  const 
+});
