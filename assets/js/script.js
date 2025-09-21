c95d28c9cc5b548b45ddc1afb0549879ce192732
LETRA <pedro.pardim.pc@gmail.com>
2025-09-20 21:47:49 -0300

    feat: add basic functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,21 @@ window.addEventListener('scroll', () => {
   const links = document.querySeclectorAll('.nav-links a');
 
   const scrollPosition = window.scrollY;
-  const 
+  const scrollThreshold = 100;
+
+  if (scrollPosition > scrollThreshold) {
+    header.style.background = 'rgba(255, 255, 255, 0.95)';
+    header.style.backdropFilter = 'blur(20px)';
+    logo.style.color = '#333';
+    links.forEach(link => {
+      link.style.color = '#333'
+    });
+  } else {
+    header.style.background = 'rgba(255, 255, 255, 0.1)';
+    header.style.backdropFilter = 'blur(10px)';
+    logo.style.color = 'white';
+    links.forEach(link => {
+      link.style.color = 'white';
+    });
+  }
 });
