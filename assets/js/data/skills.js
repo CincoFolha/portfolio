aceac864f72b77618d4aa487eccded05d417bc93
LETRA <pedro.pardim.pc@gmail.com>
2025-10-19 00:10:56 -0300

    feat: adiciona arquivo de dados de habilidades

---
+++ assets/js/data/skills.js
@@ -0,0 +1,5 @@
+const skillsData = {
+  Frontend: ["JavaScript", "TypeScript", "React", "HTML5", "CSS3"],
+  Backend: ["Node.js", "Python", "SQL", "NoSQL"],
+  DevOps: ["Gradle", "Docker", "Git", "Linux", "AWS"]
+};
