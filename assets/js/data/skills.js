6db6cd8522b3846e7dd87e48356221e7dc9919e7
LETRA <pedro.pardim.pc@gmail.com>
2025-10-28 20:41:05 -0300

    fix: remove MongoDB to backend skills list

--- assets/js/data/skills.js
+++ assets/js/data/skills.js
@@ -1,5 +1,5 @@
 const skillsData = {
   Frontend: ["JavaScript", "TypeScript", "React", "HTML5", "CSS3"],
-  Backend: ["Node.js", "Python", "SQL", "NoSQL", "SQLite", "MongoDB"],
+  Backend: ["Node.js", "Python", "SQL", "NoSQL", "SQLite"],
   DevOps: ["Gradle", "Docker", "Git", "Linux", "AWS"]
 };
