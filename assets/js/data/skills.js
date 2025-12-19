52e07ea104b220ee4edd7aaa19ff55b630254e13
LETRA <pedro.pardim.pc@gmail.com>
2025-12-19 04:45:41 -0300

    chore(skills): ajusta lista de habilidades de backend e devops

--- assets/js/data/skills.js
+++ assets/js/data/skills.js
@@ -1,5 +1,5 @@
 const skillsData = {
   Frontend: ["JavaScript", "TypeScript", "React", "HTML5", "CSS3"],
-  Backend: ["Node.js", "Python", "SQL", "NoSQL", "SQLite"],
-  DevOps: ["Gradle", "Docker", "Git", "Linux", "AWS"]
+  Backend: ["Node.js", "Python", "SQL", "SQLite"],
+  DevOps: ["Gradle", "Docker", "Git", "Linux"]
 };
